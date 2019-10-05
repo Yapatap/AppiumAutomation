20381c4381b4972db1f627d0b8dec7e8327421dd
Arsky Eugene <yapatap@gmail.com>
2019-10-05 16:23:39 +0300

    Fields-Methods

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,9 +1,24 @@
 import org.junit.Test;
 
-public class MainTest {
+public class MainTest
+{
+    int a = 5;
+    int b = 11;
     @Test
     public void myFirstTest()
     {
-        System.out.println("Hello QA engineers!");
+        int a = this.multiply(5);
+        System.out.println(a);
+        int b = this.multiply(10, 15);
+        System.out.println(b);
+    }
+    public int multiply(int number)
+    {
+        return number * 2;
+    }
+    public int multiply (int number, int multiplier)
+    {
+        return number * multiplier;
     }
 }
+
