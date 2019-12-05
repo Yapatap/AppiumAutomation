6d2113d5b3bf021975ad21b43ec628a3902ebb92
Arsky Eugene <yapatap@gmail.com>
2019-12-06 00:47:27 +0300

    Task2 is done again

---
+++ src/MainClassTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MainClassTest
+{
+        MainClass number = new MainClass();
+@Test
+    public void testGetClassNumber() {
+    if (number.getClassNumber() < 45) {
+        System.out.println("False, because number is less then 45");
+    } else if (number.getClassNumber() > 45) {
+        System.out.println("True, because number is more than 45");
+    }
+}
+
+}
